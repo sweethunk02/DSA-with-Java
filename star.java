6a2428d62a10529de76ff986780fff0c81e40bac
Anand Singh <anandsingh912344@gmail.com>
2025-09-15 17:50:46 +0530

    ss
    ss

---
+++ star.java
@@ -0,0 +1,11 @@
+// File name: Star.java
+public class star {
+    public static void main(String[] args) {
+        for (int line = 0; line < 5; line++) {
+            for (int star = 0; star < 5; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
