f294b1cd77591207f66575e2be5287c130dc93e3
Anand Singh <anandsingh912344@gmail.com>
2025-09-19 11:52:26 +0530

    s

---
+++ 1_Pattern_Question/hollow_rectangle.java
@@ -0,0 +1,17 @@
+public class hollow_rectangle {
+    public static void main(String[] args) {
+        int row = 5;
+        int col = 8;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || i == row || j == 1 || j == col) {
+
+                    System.out.print(("* "));
+                } else {
+                    System.out.print("  "); // space inside
+                }
+            }
+            System.out.println();
+        }
+    }
+}
